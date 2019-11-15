77785052a056b7454f1bbc6f02c68091c4eeee8b
ShigemiHodaka <shigemi.hodaka@simpline.co.jp>
2019-11-15 11:23:20 +0900

    add template for DB

---
+++ 03_db.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+AWS_PROFILE=""
+CHANGESET_OPTION="--no-execute-changeset"
+CFN_TEMPLATE=db.yml
+CFN_STACK_NAME=seminar-cfn-db
+
+if [ $# = 1 ] && [ $1 = "deploy" ]; then
+  echo "deploy mode"
+  CHANGESET_OPTION=""
+fi
+
+if [ -n "${AWS_PROFILE}" ];then
+    PROFILE_OPTION="--profile ${AWS_PROFILE}"
+fi
+
+# exec deploy command
+aws cloudformation deploy \
+    ${PROFILE_OPTION} \
+    --stack-name ${CFN_STACK_NAME} \
+    --template-file ${CFN_TEMPLATE} \
+    ${CHANGESET_OPTION}
