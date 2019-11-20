2881f5b937d07363e553288a2a28ff14dca2b4bd
ShigemiHodaka <shigemi.hodaka@simpline.co.jp>
2019-11-20 17:09:33 +0900

    fix security group stuffs

--- 02_security_groups.sh
+++ 02_security_groups.sh
@@ -3,7 +3,10 @@
 AWS_PROFILE=""
 CHANGESET_OPTION="--no-execute-changeset"
 CFN_TEMPLATE=security_groups.yml
-CFN_STACK_NAME=seminar-cfn-sg
+if [ -z "${NAME_PREFIX}" ];then
+    NAME_PREFIX="seminar-cfn"
+fi
+CFN_STACK_NAME="${NAME_PREFIX}-sg"
 MY_IP=$(curl globalip.me 2>/dev/null)
 
 if [ $# = 1 ] && [ $1 = "deploy" ]; then
@@ -21,4 +24,5 @@ aws cloudformation deploy \
     --stack-name "${CFN_STACK_NAME}" \
     --template-file "${CFN_TEMPLATE}" \
     --parameter-overrides MyIp="${MY_IP}/32" \
+                          NamePrefix="${NAME_PREFIX}" \
     ${CHANGESET_OPTION}
